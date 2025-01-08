72a9955a2625220ead574d50bdfed49ce55651d5
Adarsh <tomaradarsh0001gmail.com>
2025-01-09 00:04:58 +0530

    signup screen svg dynamic

--- billing-app-flutter/lib/signup.dart
+++ billing-app-flutter/lib/signup.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'login.dart';
+import 'colors.dart';
+import 'package:flutter/services.dart';  // To load the SVG as a string
 
 class SignupPage extends StatefulWidget {
   @override
@@ -11,6 +13,16 @@ class SignupPage extends StatefulWidget {
 class _SignupPageState extends State<SignupPage> {
   bool _isAnimationComplete = false;
   bool _obscureText = true;
+  String svgString = '';
+  String svgStringName = '';
+  String svgStringPass = '';
+  String svgStringEmail = '';  Color? primaryLight;
+  Color? secondaryLight; // Assuming color2 is also a dynamic color
+  Color? secondaryDark; // Assuming color2 is also a dynamic color
+  Color? primaryDark;
+  Color? svgSignup;
+  Color? links;
+  Color? textPrimary;
 
   void _togglePasswordVisibility() {
     setState(() {
@@ -21,6 +33,23 @@ class _SignupPageState extends State<SignupPage> {
   @override
   void initState() {
     super.initState();
+    AppColors.fetchColors().then((_) {
+      setState(() {
+        secondaryLight = AppColors.secondaryLight;
+        primaryLight = AppColors.primaryLight; // Replace with actual dynamic color
+        primaryDark = AppColors.primaryDark; // Replace with actual dynamic color
+        svgSignup = AppColors.svgSignup; // Replace with actual dynamic color
+        secondaryDark = AppColors.secondaryDark; // Replace with actual dynamic color
+        links = AppColors.links; // Replace with actual dynamic color
+        textPrimary = AppColors.textPrimary;
+      });
+
+      // Load SVG after colors are fetched
+      loadSvgNameIcon();
+      loadSvgPasswordIcon();
+      loadSvgEmailIcon();
+      loadSvg();
+    });
     // Start the animation after a delay
     Future.delayed(Duration(milliseconds: 200), () {
       setState(() {
@@ -28,7 +57,73 @@ class _SignupPageState extends State<SignupPage> {
       });
     });
   }
-
+  Future<void> loadSvg() async {
+    if (secondaryLight != null && primaryLight != null && primaryDark != null) {
+      String svg = await rootBundle.loadString('assets/signup_upper_shape.svg');
+      setState(() {
+        // Replace placeholders with actual colors in hex format
+        svgString = svg.replaceAll(
+          'PLACEHOLDER_COLOR_1', _colorToHex(svgSignup!),
+        ).replaceAll(
+          'PLACEHOLDER_COLOR_2', _colorToHex(secondaryDark!),
+        ).replaceAll(
+          'PLACEHOLDER_COLOR_3', _colorToHex(primaryDark!),
+        ).replaceAll(
+          'PLACEHOLDER_COLOR_4', _colorToHex(secondaryLight!),
+        ).replaceAll(
+          'PLACEHOLDER_COLOR_5', _colorToHex(secondaryDark!),
+        );
+      });
+    }
+  }
+  Future<void> loadSvgPasswordIcon() async {
+    if (secondaryDark != null && links != null && primaryDark != null) {
+      String svgpass = await rootBundle.loadString('assets/password_icon.svg');
+      setState(() {
+        // Replace placeholders with actual colors in hex format
+        svgStringPass = svgpass.replaceAll(
+          'ICON_COLOR_1', _colorToHex(links!),
+        ).replaceAll(
+          'ICON_COLOR_2', _colorToHex(textPrimary!),
+        ).replaceAll(
+          'ICON_COLOR_3', _colorToHex(primaryDark!),
+        );
+      });
+    }
+  }
+  Future<void> loadSvgEmailIcon() async {
+    if (secondaryDark != null && links != null && primaryDark != null) {
+      String svgemail = await rootBundle.loadString('assets/email_icon.svg');
+      setState(() {
+        // Replace placeholders with actual colors in hex format
+        svgStringEmail = svgemail.replaceAll(
+          'ICON_COLOR_1', _colorToHex(links!),
+        ).replaceAll(
+          'ICON_COLOR_2', _colorToHex(textPrimary!),
+        ).replaceAll(
+          'ICON_COLOR_3', _colorToHex(primaryDark!),
+        ).replaceAll(
+          'ICON_COLOR_4', _colorToHex(links!),
+        ).replaceAll(
+          'ICON_COLOR_5', _colorToHex(textPrimary!),
+        );
+      });
+    }
+  }
+  Future<void> loadSvgNameIcon() async {
+    if (secondaryDark != null && links != null && primaryDark != null) {
+      String svgname = await rootBundle.loadString('assets/name_icon.svg');
+      setState(() {
+        // Replace placeholders with actual colors in hex format
+        svgStringName = svgname.replaceAll(
+          'ICON_COLOR_1', _colorToHex(links!),
+        );
+      });
+    }
+  }
+  String _colorToHex(Color color) {
+    return '#${color.value.toRadixString(16).substring(2).toUpperCase()}';
+  }
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -44,14 +139,17 @@ class _SignupPageState extends State<SignupPage> {
           AnimatedPositioned(
             duration: Duration(milliseconds: 900),
             curve: Curves.easeInOut,
-            top: _isAnimationComplete ? -20 : -400, // Animate from -200 to 0
+            top: _isAnimationComplete ? -50 : -400, // Animate from -200 to 0
             left: 0,
             right: 0,
-            child: SvgPicture.asset(
-              'assets/signup_upper_shape.svg',
-              width: MediaQuery.of(context).size.width,
-              fit: BoxFit.cover,
-            ),
+            child: svgString.isNotEmpty
+                ? SvgPicture.string(
+              svgString,  // Render the modified SVG string with new colors
+              semanticsLabel: 'Animated and Colored SVG',
+              fit: BoxFit.fill,
+              height: 300,  // Height of the SVG
+            )
+                : CircularProgressIndicator(), // Show loading indicator until the SVG is ready
           ),
           // Main Content
           Padding(
@@ -71,7 +169,7 @@ class _SignupPageState extends State<SignupPage> {
                             style: GoogleFonts.signika(
                               fontSize: 43,
                               fontWeight: FontWeight.normal,
-                              color: const Color(0xFF649AD9),
+                              color: textPrimary,
                               height: 1.2,
                             ),
                           ),
@@ -80,7 +178,7 @@ class _SignupPageState extends State<SignupPage> {
                             style: GoogleFonts.signika(
                               fontSize: 43,
                               fontWeight: FontWeight.normal,
-                              color: const Color(0xFF649AD9),
+                              color: textPrimary,
                               height: 1.2,
                             ),
                           ),
@@ -100,11 +198,14 @@ class _SignupPageState extends State<SignupPage> {
                           decoration: InputDecoration(
                             prefixIcon: Padding(
                               padding: const EdgeInsets.only(right: 14.0),
-                              child: SvgPicture.asset(
-                                'assets/name_icon.svg',
-                                height: 15,
+                              child: svgStringName.isNotEmpty
+                                  ? SvgPicture.string(
+                                svgStringName, // Render the modified SVG string with new colors
+                                semanticsLabel: 'Animated and Colored SVG',
                                 width: 15,
-                              ),
+                                height: 15,
+                              )
+                                  : const CircularProgressIndicator(), // Show loading indicator until the SVG is ready
                             ),
                             labelText: 'Name',
                             labelStyle: GoogleFonts.signika(
@@ -130,11 +231,14 @@ class _SignupPageState extends State<SignupPage> {
                           decoration: InputDecoration(
                             prefixIcon: Padding(
                               padding: const EdgeInsets.only(right: 14.0),
-                              child: SvgPicture.asset(
-                                'assets/email_icon.svg',
-                                height: 15,
+                              child: svgStringEmail.isNotEmpty
+                                  ? SvgPicture.string(
+                                svgStringEmail, // Render the modified SVG string with new colors
+                                semanticsLabel: 'Animated and Colored SVG',
                                 width: 15,
-                              ),
+                                height: 15,
+                              )
+                                  : const CircularProgressIndicator(), // Show loading indicator until the SVG is ready
                             ),
                             labelText: 'Email',
                             labelStyle: GoogleFonts.signika(
@@ -162,11 +266,14 @@ class _SignupPageState extends State<SignupPage> {
                           decoration: InputDecoration(
                             prefixIcon: Padding(
                               padding: const EdgeInsets.only(right: 18.0),
-                              child: SvgPicture.asset(
-                                'assets/password_icon.svg',
-                                height: 25,
+                              child: svgStringPass.isNotEmpty
+                                  ? SvgPicture.string(
+                                svgStringPass, // Render the modified SVG string with new colors
+                                semanticsLabel: 'Animated and Colored SVG',
                                 width: 25,
-                              ),
+                                height: 25,
+                              )
+                                  : const CircularProgressIndicator(), // Show loading indicator until the SVG is ready
                             ),
                             suffixIcon: IconButton(
                               icon: Icon(
@@ -204,7 +311,7 @@ class _SignupPageState extends State<SignupPage> {
                             onPressed: () {},
                             style: ElevatedButton.styleFrom(
                               shape: const CircleBorder(),
-                              backgroundColor: Color(0xFF649AD9),
+                              backgroundColor: primaryDark,
                               padding: const EdgeInsets.all(18),
                             ),
                             child: const Icon(
@@ -221,7 +328,7 @@ class _SignupPageState extends State<SignupPage> {
                             'or Continue with',
                             style: TextStyle(
                               fontSize: 16,
-                              color: Color(0xFF2B48AA),
+                              color: textPrimary,
                               fontWeight: FontWeight.w500,
                             ),
                           ),
@@ -299,10 +406,10 @@ class _SignupPageState extends State<SignupPage> {
                           ),
                         );
                       },
-                      child: const Text(
+                      child: Text(
                         'Are you already a User? Login',
                         style: TextStyle(
-                          color: Color(0xFF2B48AA),
+                          color: textPrimary,
                           fontSize: 16,
                           fontWeight: FontWeight.w500,
                         ),
